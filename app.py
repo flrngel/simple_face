cc4aa207064a62ee1d5ba6f1e7e6d230c6e8e422
flrngel <flrngel@gmail.com>
2017-12-14 02:13:35 +0900

    add history, remove file from git

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import json
 import os
 import numpy as np
 
+r = redis.StrictRedis.from_url(url='redis://redis')
+
 camera_nums = 1
 visit_margin = 60 * 60 * 2
 
@@ -16,14 +18,13 @@ def get_timestamp():
 now_time = get_timestamp()
 
 #filenames = sorted(glob.glob('./cctv_images/*.jpg'))
-r = redis.StrictRedis.from_url(url='redis://redis')
 
 face_db = r.lrange('facedb', 0, -1)
 for i in range(len(face_db)):
   face_db[i] = np.asarray(json.loads(face_db[i]))
 
 for cam_i in range(camera_nums):
-  r.delete(f'bbox:{cam_i}')
+  #r.delete(f'bbox:{cam_i}')
 
   image = face_recognition.load_image_file(f'./cctv_images/{cam_i}.jpg')
   face_locations = face_recognition.face_locations(image)
@@ -36,7 +37,7 @@ for cam_i in range(camera_nums):
     if len(encodings) == 0:
       continue
     now_face = encodings[0]
-    r.zadd('bbox_history', now_time, ','.join([str(si), str(sj), str(ei), str(ej)]))
+    r.zadd(f'{cam_i}:bbox_history', now_time, ','.join([str(si), str(sj), str(ei), str(ej)]))
     if len(face_db) > 0:
       compare_face = face_recognition.api.compare_faces(face_db, now_face, tolerance=0.3)
       if True in compare_face:
@@ -47,11 +48,13 @@ for cam_i in range(camera_nums):
           last_visited = int(history[0])
           if now_time - last_visited <= visit_margin:
             r.zadd(f'{i}:history', now_time, now_time)
+            r.zadd(f'customer_history', now_time, i)
       else:
         i = len(face_db)
         r.lpush('facedb', json.dumps(now_face.tolist()))
         face_db.append(now_face[0])
         r.zadd(f'{i}:history', now_time, now_time)
+        r.zadd(f'customer_history', now_time, i)
         io.imsave(f'faces/{i}.jpg', image[si:sj, ei:ej])
     else:
       # TODO: remove duplicated code
